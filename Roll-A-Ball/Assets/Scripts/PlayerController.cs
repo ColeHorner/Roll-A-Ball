b046690fa48e3a96b2c1f8b3b96007d9af8e50fc
Cole Horner <colehorner94@gmail.com>
2018-03-21 18:20:02 -0500

    0.7
    added score and win text

--- Roll-A-Ball/Assets/Scripts/PlayerController.cs
+++ Roll-A-Ball/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();    
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -28,6 +35,15 @@ public class PlayerController : MonoBehaviour {
         if(other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
         }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+        if (count == 8)
+            winText.text = "YOU WIN!!!!";
     }
 }
